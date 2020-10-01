c3aa23b871b9dcf34ac59e68cd7714cf8a7805c9
Mansi Khamkar <khamkarmansi@gmail.com>
2020-10-01 20:55:03 +0530

    Update customheader.h

--- customheader.h
+++ customheader.h
@@ -8,15 +8,26 @@
 #include <pwd.h>
 #include <grp.h>
 #include <dirent.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 
 #define escp 27
-#define clsr printf("%c[2J", escp)
+unsigned int xcor = 1, ycor = 1;
+#define esc 27
+#define cls printf("%c[2J", esc)
+#define pos() printf("\033[%d;%df",xcor,ycor)
 
-extern stack<string> forward_stack;
-extern stack<string> back_stack;
-//extern vector<string> cur_files;
-extern vector<dirent> cur_dirs;
+struct winsize terminal;
+unsigned int term_row;
+unsigned int term_col;
+int cur_window = 0;
+string cur_dir;
+string app_home;
+stack<string> forward_stack;
+stack<string> back_stack;
+vector<dirent> cur_dirs;
 
 int list_files();
+void update_list(string);
+void travel(string);
