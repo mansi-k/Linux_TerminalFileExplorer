53274ad8cd8edadc5d075d3e51f3c6ec24ac7501
Mansi Khamkar <khamkarmansi@gmail.com>
2020-10-02 21:46:35 +0530

    Update customheader.h

--- customheader.h
+++ customheader.h
@@ -18,6 +18,7 @@ unsigned int xcor = 1, ycor = 1;
 #define cls printf("%c[2J", esc)
 #define cursor printf("\033[%d;%df",xcor,ycor)
 #define setcout(cx,cy) printf("\033[%d;%df",cx,cy)
+#define clr_line printf("%c[2K", 27)
 
 bool cmd_mode = false;
 struct winsize terminal;
@@ -29,6 +30,8 @@ string app_home;
 stack<string> forward_stack;
 stack<string> back_stack;
 vector<dirent> cur_dirs;
+int scr_rows;
+int scr_cols;
 
 void list_files();
 void update_list();
@@ -36,3 +39,4 @@ void travel();
 void set_termios();
 void reset_termios();
 void command_mode();
+void execute_cmd(string);
